9a625d76823022f34e005c07287177ef5942ca1d
Guilherme <guilhermersouza34@gmail.com>
2024-03-05 13:07:51 -0300

    Ex7

---
+++ Media.js
@@ -0,0 +1,4 @@
+var nota1 = parseFloat(prompt("Digite a primeira nota:"));
+var nota2 = parseFloat(prompt("Digite a segunda nota:"));
+var media = (nota1 + nota2) / 2;
+alert("A média do aluno é: " + media);
