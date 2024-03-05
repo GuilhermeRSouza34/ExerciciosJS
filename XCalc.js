95dde67fcaa65fec939e40c6cba5d714e51cbb6d
Guilherme <guilhermersouza34@gmail.com>
2024-03-05 13:08:19 -0300

    Ex8

---
+++ XCalc.js
@@ -0,0 +1,9 @@
+var num1 = 10;
+var num2 = 5;
+
+var soma = num1 + num2;
+var subtracao = num1 - num2;
+var multiplicacao = num1 * num2;
+var divisao = num1 / num2;
+
+alert("Soma: " + soma + "\nSubtração: " + subtracao + "\nMultiplicação: " + multiplicacao + "\nDivisão: " + divisao);
