853ff0333b4115ea4616c9e502bf9a428c2503fa
Guilherme <guilhermersouza34@gmail.com>
2024-03-13 23:12:49 -0300

    Transformar Celsius em Fahrenheit

---
+++ CelsiusFahrenheit.js
@@ -0,0 +1,8 @@
+// Função para converter temperatura de Celsius para Fahrenheit
+function celsiusParaFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+// Exemplo de uso
+console.log(celsiusParaFahrenheit(0)); // Saída: 32
+console.log(celsiusParaFahrenheit(100)); // Saída: 212
