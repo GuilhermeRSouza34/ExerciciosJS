46d18d2a6e82cad4a53a9f0502c8b90e67725b7c
Guilherme <guilhermersouza34@gmail.com>
2024-03-14 22:44:21 -0300

    Além de criar e fazer login, adicionei a capacidade de exibir todos os logins existentes, atualizar um login existente e excluir um login.

--- Login2.js
+++ Login2.js
@@ -7,8 +7,12 @@ function criarLogin() {
     var password = prompt("Digite uma senha:");
 
     if (username && password) {
-        logins[username] = password;
-        alert("Login criado com sucesso!");
+        if (!logins.hasOwnProperty(username)) {
+            logins[username] = password;
+            alert("Login criado com sucesso!");
+        } else {
+            alert("O nome de usuário já está em uso. Por favor, escolha outro.");
+        }
     } else {
         alert("Nome de usuário e senha são necessários para criar um login.");
     }
@@ -26,9 +30,41 @@ function fazerLogin() {
     }
 }
 
+// Função para exibir todos os logins existentes
+function exibirLogins() {
+    var lista = "Logins existentes:\n";
+    for (var user in logins) {
+        lista += user + "\n";
+    }
+    alert(lista);
+}
+
+// Função para atualizar um login existente
+function atualizarLogin() {
+    var username = prompt("Digite o nome de usuário que deseja atualizar:");
+    if (logins.hasOwnProperty(username)) {
+        var newPassword = prompt("Digite a nova senha:");
+        logins[username] = newPassword;
+        alert("Senha atualizada com sucesso!");
+    } else {
+        alert("Nome de usuário não encontrado.");
+    }
+}
+
+// Função para excluir um login
+function excluirLogin() {
+    var username = prompt("Digite o nome de usuário que deseja excluir:");
+    if (logins.hasOwnProperty(username)) {
+        delete logins[username];
+        alert("Login excluído com sucesso!");
+    } else {
+        alert("Nome de usuário não encontrado.");
+    }
+}
+
 // Função para exibir o menu
 function exibirMenu() {
-    var opcao = prompt("Escolha uma opção:\n 1. Criar login\n 2. Fazer login\n 3. Sair");
+    var opcao = prompt("Escolha uma opção:\n 1. Criar login\n 2. Fazer login\n 3. Exibir todos os logins\n 4. Atualizar login\n 5. Excluir login\n 6. Sair");
 
     switch (opcao) {
         case "1":
@@ -38,6 +74,15 @@ function exibirMenu() {
             fazerLogin();
             break;
         case "3":
+            exibirLogins();
+            break;
+        case "4":
+            atualizarLogin();
+            break;
+        case "5":
+            excluirLogin();
+            break;
+        case "6":
             alert("Até logo!");
             break;
         default:
